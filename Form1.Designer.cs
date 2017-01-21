679139e1c7baef30405f2765943c0a7c25acbe5e
dsthilaire <david.st.hilaire@pnnl.gov>
2017-01-21 02:42:44 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Mp3ToUsb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.TextBox textBox1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Button btnBrowseDest;
+            System.Windows.Forms.GroupBox groupBox1;
+            System.Windows.Forms.Label label3;
+            System.Windows.Forms.GroupBox groupBox2;
+            System.Windows.Forms.Button btnRun;
+            System.Windows.Forms.Button btnRemove;
+            System.Windows.Forms.Label label4;
+            System.Windows.Forms.Label label5;
+            System.Windows.Forms.Label label6;
+            System.Windows.Forms.Label label7;
+            System.Windows.Forms.TextBox textBox2;
+            this.txtDestPath = new System.Windows.Forms.TextBox();
+            this.txtSeparator = new System.Windows.Forms.TextBox();
+            this.lbFiles = new System.Windows.Forms.ListBox();
+            this.txtFolderStructure = new System.Windows.Forms.TextBox();
+            this.txtFilenameFormat = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            textBox1 = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            btnBrowseDest = new System.Windows.Forms.Button();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            label3 = new System.Windows.Forms.Label();
+            groupBox2 = new System.Windows.Forms.GroupBox();
+            btnRun = new System.Windows.Forms.Button();
+            btnRemove = new System.Windows.Forms.Button();
+            label4 = new System.Windows.Forms.Label();
+            label5 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            label7 = new System.Windows.Forms.Label();
+            textBox2 = new System.Windows.Forms.TextBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(6, 51);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(145, 13);
+            label1.TabIndex = 0;
+            label1.Text = "Input filename field separator:";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            textBox1.Location = new System.Drawing.Point(29, 128);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new System.Drawing.Size(256, 61);
+            textBox1.TabIndex = 2;
+            textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(6, 285);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(113, 13);
+            label2.TabIndex = 7;
+            label2.Text = "Destination root folder:";
+            // 
+            // btnBrowseDest
+            // 
+            btnBrowseDest.Location = new System.Drawing.Point(125, 280);
+            btnBrowseDest.Name = "btnBrowseDest";
+            btnBrowseDest.Size = new System.Drawing.Size(75, 23);
+            btnBrowseDest.TabIndex = 8;
+            btnBrowseDest.Text = "Browse";
+            btnBrowseDest.UseVisualStyleBackColor = true;
+            btnBrowseDest.Click += new System.EventHandler(this.btnBrowseDest_Click);
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(this.txtFilenameFormat);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(this.txtFolderStructure);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(this.txtDestPath);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(btnBrowseDest);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(this.txtSeparator);
+            groupBox1.Location = new System.Drawing.Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new System.Drawing.Size(291, 357);
+            groupBox1.TabIndex = 10;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Settings";
+            // 
+            // txtDestPath
+            // 
+            this.txtDestPath.Location = new System.Drawing.Point(24, 309);
+            this.txtDestPath.Name = "txtDestPath";
+            this.txtDestPath.ReadOnly = true;
+            this.txtDestPath.Size = new System.Drawing.Size(233, 20);
+            this.txtDestPath.TabIndex = 9;
+            // 
+            // txtSeparator
+            // 
+            this.txtSeparator.Location = new System.Drawing.Point(157, 48);
+            this.txtSeparator.Name = "txtSeparator";
+            this.txtSeparator.Size = new System.Drawing.Size(128, 20);
+            this.txtSeparator.TabIndex = 1;
+            this.txtSeparator.Text = "-";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(6, 24);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(269, 13);
+            label3.TabIndex = 12;
+            label3.Text = "Drag and drop files from Windows Explorer onto list box.";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnRun);
+            groupBox2.Controls.Add(btnRemove);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(this.lbFiles);
+            groupBox2.Location = new System.Drawing.Point(309, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new System.Drawing.Size(571, 357);
+            groupBox2.TabIndex = 13;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Input Files";
+            // 
+            // btnRun
+            // 
+            btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            btnRun.Location = new System.Drawing.Point(430, 328);
+            btnRun.Name = "btnRun";
+            btnRun.Size = new System.Drawing.Size(135, 23);
+            btnRun.TabIndex = 14;
+            btnRun.Text = "Copy Files";
+            btnRun.UseVisualStyleBackColor = true;
+            btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnRemove
+            // 
+            btnRemove.Location = new System.Drawing.Point(296, 328);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new System.Drawing.Size(128, 23);
+            btnRemove.TabIndex = 13;
+            btnRemove.Text = "Remove Selected";
+            btnRemove.UseVisualStyleBackColor = true;
+            btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // lbFiles
+            // 
+            this.lbFiles.AllowDrop = true;
+            this.lbFiles.FormattingEnabled = true;
+            this.lbFiles.Location = new System.Drawing.Point(6, 40);
+            this.lbFiles.Name = "lbFiles";
+            this.lbFiles.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lbFiles.Size = new System.Drawing.Size(559, 277);
+            this.lbFiles.Sorted = true;
+            this.lbFiles.TabIndex = 11;
+            this.lbFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.lbFiles_DragDrop);
+            this.lbFiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.lbFiles_DragEnter);
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(6, 24);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(252, 13);
+            label4.TabIndex = 10;
+            label4.Text = "Sample filename format:  Artist - Album - Track - Title";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(26, 76);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(125, 13);
+            label5.TabIndex = 11;
+            label5.Text = "In sample separator is \"-\"";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new System.Drawing.Point(6, 105);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(99, 13);
+            label6.TabIndex = 12;
+            label6.Text = "Heirarchy structure:";
+            // 
+            // txtFolderStructure
+            // 
+            this.txtFolderStructure.Location = new System.Drawing.Point(111, 102);
+            this.txtFolderStructure.Name = "txtFolderStructure";
+            this.txtFolderStructure.Size = new System.Drawing.Size(174, 20);
+            this.txtFolderStructure.TabIndex = 13;
+            this.txtFolderStructure.Text = "1/2";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new System.Drawing.Point(6, 198);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(116, 13);
+            label7.TabIndex = 14;
+            label7.Text = "Output filename format:";
+            // 
+            // txtFilenameFormat
+            // 
+            this.txtFilenameFormat.Location = new System.Drawing.Point(125, 195);
+            this.txtFilenameFormat.Name = "txtFilenameFormat";
+            this.txtFilenameFormat.Size = new System.Drawing.Size(160, 20);
+            this.txtFilenameFormat.TabIndex = 15;
+            this.txtFilenameFormat.Text = "3 - 4";
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            textBox2.Location = new System.Drawing.Point(29, 221);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new System.Drawing.Size(256, 51);
+            textBox2.TabIndex = 16;
+            textBox2.Text = "Output filename defined by field indexes.  Using the sample format, entering \"3 -" +
+    " 4\" will create output filenames of the format Track - Title.\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 381);
+            this.Controls.Add(groupBox2);
+            this.Controls.Add(groupBox1);
+            this.Name = "Form1";
+            this.Text = "Mp3 2 USB";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSeparator;
+        private System.Windows.Forms.TextBox txtDestPath;
+        private System.Windows.Forms.ListBox lbFiles;
+        private System.Windows.Forms.TextBox txtFilenameFormat;
+        private System.Windows.Forms.TextBox txtFolderStructure;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Mp3ToUsb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseDest_Click(object sender, EventArgs e)
+        {
+            var dlg = new FolderBrowserDialog();
+            dlg.ShowNewFolderButton = true;
+            dlg.RootFolder = Environment.SpecialFolder.Desktop;
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtDestPath.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void lbFiles_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void lbFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            // Ensure that the list item index is contained in the data. 
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var files = e.Data.GetData(DataFormats.FileDrop) as string[];
+                lbFiles.Items.AddRange(files.Distinct().Where(f => !lbFiles.Items.Contains(f)).ToArray());
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lbFiles.BeginUpdate();
+                for (int i = lbFiles.SelectedItems.Count - 1; i >= 0; i--)
+                    lbFiles.Items.Remove(lbFiles.SelectedItems[i]);
+            }
+            finally
+            {
+                lbFiles.EndUpdate();
+            }
+        }
+
+        class InputFile
+        {
+            public static string FilenameFormat { get; set; }
+            public string FilePath { get; set; }
+            public List<string> Fields { get; set; }
+            private string _filename = null;
+            public string FileName
+            {
+                get
+                {
+                    if (_filename == null)
+                    {
+                        if (string.IsNullOrWhiteSpace(FilenameFormat))
+                            _filename = Path.GetFileName(FilePath);
+                        else
+                        {
+                            _filename = FilenameFormat;
+                            for (int i = Fields.Count - 1; i >= 0; i--)
+                                _filename = _filename.Replace("*" + (i + 1).ToString() + "*", Fields[i]);
+                            // any tokens that weren't replaced must be numbers, not tokens, so get rid of '*'
+                            _filename = _filename.Replace("*", "") + Path.GetExtension(FilePath);
+                        }
+                    }
+                    return _filename;
+                }
+            }
+        }
+
+        enum Overwrite { Undefined, Yes, No, Prompt }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (!GoodToGo())
+                return;
+
+            // Get directory structure
+            var parts = txtFolderStructure.Text.Split('\\', '/').Where(x => !string.IsNullOrWhiteSpace(x));
+            int n, nMax = 0;
+            List<int> folders = new List<int>();
+            foreach (var part in parts)
+            {
+                if (!int.TryParse(part, out n))
+                {
+                    MessageBox.Show("Invalid directory structure.  Error at '" + part + "'");
+                    return;
+                }
+                folders.Add(n);
+                if (n > nMax)
+                    nMax = n;
+            }
+
+            // init some other stuff
+            var sep = new string [] { txtSeparator.Text };
+            InputFile.FilenameFormat = GetTokenizedFilenameFormat();
+            if (InputFile.FilenameFormat.Length > 0 && !InputFile.FilenameFormat.Contains('*'))
+            {
+                MessageBox.Show("Invalid output filename format.  No field indexes specified!");
+                return;
+            }
+
+            var overwrite = Overwrite.Undefined;
+
+            // Build list of input file objects
+            var files = from li in lbFiles.Items.Cast<string>()
+                        select new InputFile
+                        {
+                            FilePath = li,
+                            Fields = Path.GetFileNameWithoutExtension(li)
+                                        .Split(sep, StringSplitOptions.None)
+                                        .Select(x => x.Trim())
+                                        .ToList()
+                        };
+
+            var filelist = files.ToList();
+            if (files.Any(x => x.Fields.Count < nMax))
+            {
+                MessageBox.Show("Filenames found with fewer fields than required by the Heirarchy Structure.");
+                return;
+            }
+            // sort by each folder level from the top down, then finally by filename.  These must be applied in reverse order
+            files = files.OrderBy(f => f.FileName);
+            foreach (int i in folders.Reverse<int>())
+            {
+                int index = i-1;
+                files = files.OrderBy(f => f.Fields[index]);
+            }
+            filelist = files.ToList();
+
+            var sb = new StringBuilder(256);
+            string folder;
+            foreach (var f in files)
+            {
+                sb.Clear();
+                sb.Append(txtDestPath.Text + "\\");
+                foreach (int i in folders)
+                    sb.Append(f.Fields[i-1] + "\\");
+                folder = sb.ToString();
+                if (!Directory.Exists(folder))
+                    Directory.CreateDirectory(folder);
+                folder = folder + f.FileName;
+                bool bCopy = true;
+                if (File.Exists(folder))
+                {
+                    if (overwrite == Overwrite.No)
+                        bCopy = false;
+                    else if (overwrite == Overwrite.Undefined || overwrite == Overwrite.Prompt)
+                    {
+                        switch (MessageBox.Show("Overwrite " + folder + "?", "File Exists", MessageBoxButtons.YesNoCancel))
+                        {
+                            case System.Windows.Forms.DialogResult.Cancel: return;
+                            case System.Windows.Forms.DialogResult.No: bCopy = false; break;
+                            case System.Windows.Forms.DialogResult.Yes: bCopy = true; break;
+                            default: MessageBox.Show("Invalid button??"); return;
+                        }
+
+                        if (overwrite == Overwrite.Undefined)
+                        {
+                            if (MessageBox.Show("Apply this action to all duplicates?", "Remember answer?", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                                overwrite = (bCopy ? Overwrite.Yes : Overwrite.No);
+                            else
+                                overwrite = Overwrite.Prompt;
+                        }
+                    }
+                }
+                if (bCopy)
+                {
+                    try
+                    {
+                        File.Copy(f.FilePath, folder, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Exception copying to " + folder + ":\n" + ex.ToString());
+                    }
+                }
+            }
+
+            if (MessageBox.Show("Operation completed.  Clear Input Files list?", "Done", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                lbFiles.Items.Clear();
+        }
+
+        private string GetTokenizedFilenameFormat()
+        {
+            //  Take the filename format and wrap any numbers in it with '*'.  '*' cannot be used in filenames, so there should be no conflicts.
+            var chars = txtFilenameFormat.Text.ToList();
+            bool bInNumber = false;
+            for (int i = 0; i < chars.Count; i++)
+            {
+                bool bIsNumber = (chars[i] >= '0' && chars[i] <= '9');
+                if (bIsNumber != bInNumber)
+                {
+                    chars.Insert(i, '*');
+                    bInNumber = !bInNumber;
+                    i++;
+                }
+            }
+            if (bInNumber)
+                chars.Add('*');
+            return new string(chars.ToArray());
+        }
+
+        private bool GoodToGo()
+        {
+            if (txtSeparator.Text.Trim().Length == 0 && txtFolderStructure.Text.Trim().Length > 0)
+            {
+                MessageBox.Show("If Heirarchy Structure is specified, a separator must also be specified.");
+                return false;
+            }
+            if (txtSeparator.Text.Trim().Length == 0 && txtFilenameFormat.Text.Trim().Length > 0)
+            {
+                MessageBox.Show("If Output Filename Format is specified, a separator must also be specified.");
+                return false;
+            }
+            if (txtDestPath.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("A Destination Root Folder must be selected.");
+                return false;
+            }
+            if (lbFiles.Items.Count == 0)
+            {
+                MessageBox.Show("No Input Files have been added.");
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+}
